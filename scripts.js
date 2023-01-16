0b40a9cc852594a0f6a57e25380db3fa07b59dc0
andreamabel <110324261+andreamabel@users.noreply.github.com>
2023-01-16 00:15:36 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,3 @@
 // window.location.href = ‘https://www.facebook.com’;
-window.location.assign(‘https://www.facebook.com’);
+// window.location.assign(‘https://www.facebook.com’);
+setTimeout( function() { window.location.assign = "https://www.facebook.com"; }, 5000 );
